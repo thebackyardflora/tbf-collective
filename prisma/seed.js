f3d64b679d4411c25a7a9e4d5c665e85fbbc00f9
Justin Waite <justindwaite@gmail.com>
2022-06-23 23:57:24 -0600

    fix seed script

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,5 +1,5 @@
 const { PrismaClient } = require('@prisma/client');
-const bcrypt = require('bcryptjs');
+const faker = require('@faker-js/faker');
 
 const prisma = new PrismaClient();
 
@@ -11,32 +11,10 @@ async function seed() {
     // no worries if it doesn't exist yet
   });
 
-  const hashedPassword = await bcrypt.hash('racheliscool', 10);
-
-  const user = await prisma.user.create({
+  await prisma.user.create({
     data: {
       email,
-      password: {
-        create: {
-          hash: hashedPassword,
-        },
-      },
-    },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: 'My first note',
-      body: 'Hello, world!',
-      userId: user.id,
-    },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: 'My second note',
-      body: 'Hello, world!',
-      userId: user.id,
+      externalId: faker.datatype.string(),
     },
   });
 
