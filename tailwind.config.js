9b56dafaa6817e43f28fd01096bd9a3e9b32af55
Justin Waite <justindwaite@gmail.com>
2022-06-30 09:15:26 -0600

    simplify home page so hopefully cypress tests pass

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{ts,tsx,jsx,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#78866B',
+          50: '#D4D9D0',
+          100: '#CAD0C4',
+          200: '#B5BEAD',
+          300: '#A1AC97',
+          400: '#8C9A80',
+          500: '#78866B',
+          600: '#5C6752',
+          700: '#404839',
+          800: '#242820',
+          900: '#080907',
+        },
+        danger: colors.red,
+      },
+    },
   },
   plugins: [],
 };
