d7b90e687ba7bcd7bf8bd0c4c89f1fca4e289ff8
Justin Waite <justindwaite@gmail.com>
2022-07-04 16:35:27 -0600

    update the index page with a header and hero section

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,20 +1,37 @@
-import AgricultureIcon from '@mui/icons-material/Agriculture';
-import LocalFloristIcon from '@mui/icons-material/LocalFlorist';
+import Header from '~/components/Header';
+import { Link } from '@remix-run/react';
+import { Button } from '@mando-collabs/tailwind-ui';
 
 export default function Index() {
   return (
-    <main className="relative flex min-h-screen items-center justify-center bg-white">
-      <div className="relative w-full sm:pb-16 sm:pt-8">
-        <div className="mx-auto w-full max-w-md space-y-4 sm:px-6 lg:px-8">
-          <button className="flex w-full items-center justify-center rounded-lg border-2 border-primary-500 p-4 font-bold text-primary-600 hover:bg-primary-50 sm:p-8">
-            I'm a florist <LocalFloristIcon />
-          </button>
-
-          <button className="flex w-full items-center justify-center rounded-lg border-2 border-primary-500 p-4 sm:p-8">
-            I'm a grower <AgricultureIcon />
-          </button>
+    <>
+      <Header />
+      <main className="mx-auto mt-16 max-w-7xl px-4 sm:mt-24">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+            <span className="block xl:inline">Support</span>{' '}
+            <span className="block text-primary-600 xl:inline">locally grown flowers</span>
+          </h1>
+          <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
+            Join our southeast Idaho collective of local growers and florists. As a member of the collective, you will
+            have access to a weekly wholesale floor.
+          </p>
+          <div className="mx-auto mt-5 max-w-md sm:flex sm:justify-center md:mt-8">
+            <Link to="#">
+              <Button className="w-full" size="xl">
+                Apply now
+              </Button>
+            </Link>
+            <div className="mt-3 sm:mt-0 sm:ml-3">
+              <Link to="#">
+                <Button className="w-full" kind="white" size="xl">
+                  Contact us
+                </Button>
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
