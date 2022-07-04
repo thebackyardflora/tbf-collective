140b1eab397b77e0019d2907cf96940709117f7a
Justin Waite <justindwaite@gmail.com>
2022-07-03 21:12:25 -0600

    use node_env test in crypess action e2e test

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,6 +1,8 @@
 import { RemixBrowser } from '@remix-run/react';
 import { hydrateRoot } from 'react-dom/client';
 
+console.log('Hydrating in env:', process.env.NODE_ENV);
+
 if (process.env.NODE_ENV === 'test') {
   require('react-dom').hydrate(<RemixBrowser />, document);
 } else {
