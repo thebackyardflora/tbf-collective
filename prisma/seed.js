00f184a46bd669a6851285153490c0b4d5431f6b
Justin Waite <justindwaite@gmail.com>
2022-07-08 14:58:31 -0600

    update header to be reactive to user login state

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,14 +4,14 @@ const bcrypt = require('bcryptjs');
 const prisma = new PrismaClient();
 
 async function seed() {
-  const email = 'rachel@remix.run';
+  const email = 'user1@example.com';
 
   // cleanup the existing database
   await prisma.user.delete({ where: { email } }).catch(() => {
     // no worries if it doesn't exist yet
   });
 
-  const hashedPassword = await bcrypt.hash('racheliscool', 10);
+  const hashedPassword = await bcrypt.hash('password', 10);
 
   await prisma.user.create({
     data: {
