077f21857a92a03caf6a2513472e36b057c6485f
Justin Waite <justindwaite@gmail.com>
2022-08-03 20:23:20 -0600

    allow video recording

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
       const port = process.env.PORT ?? (isDev ? '3000' : '8811');
       const configOverrides: Partial<Cypress.PluginConfigOptions> = {
         baseUrl: `http://localhost:${port}`,
-        video: !process.env.CI,
       };
 
       // To use this:
