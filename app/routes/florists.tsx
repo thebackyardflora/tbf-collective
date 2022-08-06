7525243eea19a703a7ee71ba2a6e7f59b2b56ee4
Justin Waite <justindwaite@gmail.com>
2022-08-05 20:08:03 -0600

    feat: add florists dashboard

--- app/routes/florists.tsx
+++ app/routes/florists.tsx
@@ -1,31 +1,33 @@
 import type { LoaderArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { requireActiveCompany } from '~/session.server';
-
-import { Outlet, useLoaderData } from '@remix-run/react';
 import { CompanyType } from '@prisma/client';
-import { AppLayout } from '~/components/layout/AppLayout';
 import { getImageUrl } from '~/cloudinary.server';
+import { AppLayout } from '~/components/layout/AppLayout';
+import { Outlet, useLoaderData } from '@remix-run/react';
+import { PageWrapper } from '~/components/PageWrapper';
 
 export async function loader({ request }: LoaderArgs) {
-  const { user, company } = await requireActiveCompany(request, CompanyType.GROWER);
+  const { user, company } = await requireActiveCompany(request, CompanyType.FLORIST);
 
   return json({
     user: {
       name: user.name,
       email: user.email,
-      imageUrl: company.imageKey ? getImageUrl(company.imageKey, { width: 150, height: 150 }) : null,
+      imageUrl: company.imageKey ? getImageUrl(company.imageKey) : null,
       isAdmin: user.isAdmin,
     },
   });
 }
 
-export default function GrowersLayout() {
+export default function FloristsLayout() {
   const { user } = useLoaderData<typeof loader>();
 
   return (
     <AppLayout user={user}>
-      <Outlet />
+      <PageWrapper title="Florist Dashboard" description="Check the upcoming market and search growers' inventory.">
+        <Outlet />
+      </PageWrapper>
     </AppLayout>
   );
 }
